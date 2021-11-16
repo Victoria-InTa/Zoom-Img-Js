336e7c2e47bc4e41e6d15390b82da4f1af5b4fe4
Victoria_T <Tabordaingridvictoria@outlook.es>
2021-11-16 18:34:40 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let seccion1 = document.querySelector('section')
+let seccion2 = document.querySelector('section')
+seccion1.onclick = function (){
+    seccion2.classList.toggle('agrandar')
+    
+}
+
+
